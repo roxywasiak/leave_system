c33911aec71aeea02f208b7dcd6e6c2e06803c32
Rukhsana Wasiak <ruksclone@hotmail.com>
2025-03-28 15:00:02 +0000

    update config and create express server

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.send("Leave System API is running...");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
