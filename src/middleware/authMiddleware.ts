b151aadba10ee13713dd027fe6e7d6068965d24e
Rukhsana Wasiak <ruksclone@hotmail.com>
2025-04-10 13:47:15 +0100

    token

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -2,5 +2,22 @@ import {Request, Response, NextFunction} from 'express';
 import jwt from 'jsonwebtoken'; 
 
 export interface AuthRequest extends Request {
-    user?: { id: number; email: string };//TOKEN 
+    user?: any; 
 }
+export const verifyToken = (req: AuthRequest, res: Response, next: NextFunction) => {
+    const authHeader = req.headers.authorization;
+  
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({ message: "Access denied. No token provided." });
+    }
+  
+    const token = authHeader.split(" ")[1];
+  
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET || "secret");
+      req.user = decoded; 
+      next();
+    } catch (err) {
+      return res.status(403).json({ message: "Invalid or expired token." });
+    }
+  };
