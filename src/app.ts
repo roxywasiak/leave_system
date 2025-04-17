86a91047917c3698cc092704d5dcf8bdc7093c67
Rukhsana Wasiak <ruksclone@hotmail.com>
2025-04-17 10:10:34 +0100

    moved the  middleware as the npm keept crashing

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,33 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import authRoutes from './routes/authRoutes';
-import leaveRoutes from './routes/leaveRoutes';
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import authRoutes from "./routes/authRoutes";
+import leaveRoutes from "./routes/leaveRoutes";
+import pool from "./config/db";
 
-dotenv.config();
 const app = express();
-app.use(express.json());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/leave', leaveRoutes);
+app.use(express.json());
+app.use(cors());
+app.use(helmet())
+
+app.use("/api/auth", authRoutes);
+app.use("/api/leave", leaveRoutes);
+
+
+app.get("/", (req, res) => {
+  res.send("Leave System API is running...");
+});
+
+app.get("/api/test-db", async (req, res) => {
+  try {
+    const [rows] = await pool.query("SELECT NOW()");
+    res.json({ success: true, time: (rows as any)[0]["NOW()"] });
+  } catch (err) {
+    console.error("DB Error:", err);
+    res.status(500).json({ success: false, message: "DB connection failed" });
+  }
+});
 
 export default app;
+
--- src/server.ts
+++ src/server.ts
@@ -1,36 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors";
-import helmet from "helmet";
-import pool from "./config/db"; 
-import leaveRoutes from "./routes/leaveRoutes"; 
-import authRoutes from "./routes/authRoutes"; 
+
 
 dotenv.config();
 
 const app = express();
 
-app.use(express.json());
-app.use(cors());
-app.use(helmet());
-
 const PORT = process.env.PORT || 5000;
-app.use("/api/leave", leaveRoutes);
-app.use("/api/auth", authRoutes);
-
-app.get("/", (req, res) => {
-  res.send("Leave System API is running...");
-});
-
-app.get("/api/test-db", async (req, res) => {
-  try {
-    const [rows] = await pool.query("SELECT NOW()");
-    res.json({ success: true, time: (rows as any)[0]["NOW()"] });
-  } catch (err) {
-    console.error("DB Error:", err);
-    res.status(500).json({ success: false, message: "DB connection failed" });
-  }
-});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
